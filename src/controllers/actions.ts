f7f1ced1070118e10f3cf5509695d46cfe0fc1a5
AryanKarim <aryanmajid97@gmail.com>
2023-07-15 20:40:41 +0300

    refactor put listeners in listers.ts

--- src/controllers/actions.ts
+++ src/controllers/actions.ts
@@ -1,35 +1,4 @@
-// @ts-ignore
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { camera, renderer } from "../environment/renderer";
-//__________________________________ WINDOW RESIZE _________________________________________
-
-function resizeRendererToDisplaySize(renderer: any) {
-  const canvas = renderer.domElement;
-  const width = canvas.clientWidth;
-  const height = canvas.clientHeight;
-
-  const needResize = canvas.width !== width || canvas.height !== height;
-  if (needResize) {
-    renderer.setSize(width, height, false);
-  }
-
-  return needResize;
-}
-
-const checkForResize = () => {
-  if (resizeRendererToDisplaySize(renderer)) {
-    const canvas = renderer.domElement;
-    camera.camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.camera.updateProjectionMatrix();
-  }
-};
-
-const controls = new OrbitControls(camera.camera, renderer.domElement);
-
-controls.target.set(0, 0.5, 0);
-
-controls.update();
-controls.enablePan = false;
+import { checkForResize, controls } from "./listeners";
 
 export const actions = () => {
   checkForResize();
--- src/controllers/listeners.ts
+++ src/controllers/listeners.ts
@@ -1,7 +1,9 @@
-//__________________________________ AUDIO _________________________________________
-
+// @ts-ignore
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { camera, renderer } from "../environment/renderer";
 import { bars, lines } from "../entities/bars";
 
+//__________________________________ AUDIO _________________________________________
 let audioContext: any;
 let mediaStream: any;
 let audioSource: any;
@@ -49,3 +51,32 @@ function processAudio(event: any) {
 }
 (window as any).startRecording = startRecording;
 (window as any).stopRecording = stopRecording;
+
+//__________________________________ WINDOW RESIZE _________________________________________
+function resizeRendererToDisplaySize(renderer: any) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+
+  return needResize;
+}
+
+export const checkForResize = () => {
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.camera.updateProjectionMatrix();
+  }
+};
+
+export const controls = new OrbitControls(camera.camera, renderer.domElement);
+
+controls.target.set(0, 0.5, 0);
+
+controls.update();
+controls.enablePan = false;
