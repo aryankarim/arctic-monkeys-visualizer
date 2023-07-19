e9511a8d7217dd566e20427caf4a8d7bfeeab4b4
AryanKarim <aryanmajid97@gmail.com>
2023-07-19 20:51:40 +0300

    add moving waves

--- src/controllers/actions/main.ts
+++ src/controllers/actions/main.ts
@@ -1,6 +1,71 @@
+// import { n, skeleton, tapeworm } from "../../entities/wave";
+import { scene } from "../../environment/renderer";
 import { checkForResize, controls } from "../listeners/window";
+import * as THREE from "three";
+var clock = new THREE.Clock(true);
+
+let tubeGeometry, tubeMesh;
+const numPoints = 1000;
+const amplitude = 1;
+const frequency = 3;
+const points = [];
+
+// Create the initial path for the tube geometry
+for (let i = 0; i < numPoints; i++) {
+  const x = (i / numPoints) * 4 * Math.PI;
+  const y = amplitude * Math.sin(frequency * x);
+  const z = 0;
+  points.push(new THREE.Vector3(x, y, z));
+}
+
+tubeGeometry = new THREE.TubeGeometry(
+  new THREE.CatmullRomCurve3(points),
+  numPoints - 1,
+  0.04,
+  8,
+  false
+);
+const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+tubeMesh = new THREE.Mesh(tubeGeometry, material);
+scene.add(tubeMesh);
 
 export const actions = () => {
+  // Move the points to their new positions
+  for (let i = 0; i < numPoints; i++) {
+    points[i].x += 0.01; // Example: move the points along the x-axis
+    points[i].y = amplitude * Math.sin(frequency * points[i].x);
+  }
+
+  // Recompute the tube geometry with the updated points
+  tubeGeometry = new THREE.TubeGeometry(
+    new THREE.CatmullRomCurve3(points),
+    numPoints - 1,
+    0.04,
+    8,
+    false
+  );
+  tubeMesh.geometry.dispose(); // Clean up the previous geometry
+  tubeMesh.geometry = tubeGeometry; // Assign the new geometry to the mesh
+  // var time = clock.getElapsedTime();
+  // // tapeworm motion
+
+  // tapeworm.rotation.z = -0.1 * Math.cos(1.9 * time);
+  // tapeworm.position.y = -10.2 * Math.cos(1.9 * time);
+
+  // for (var i = 1; i <= n; i++) {
+  //   skeleton.bones[i].rotation.x = THREE.MathUtils.degToRad(
+  //     i * 1 * Math.cos(0.2 * time + i * i)
+  //   );
+  //   skeleton.bones[i].rotation.z = THREE.MathUtils.degToRad(
+  //     i * 5 * Math.sin(1.9 * time - i)
+  //   );
+  // }
+
+  // skeleton.bones[0].rotation.z = THREE.MathUtils.degToRad(
+  //   20 * Math.cos((1.9 / 2) * time)
+  // );
+  // skeleton.bones[1].rotation.z = -skeleton.bones[0].rotation.z / 2;
+  // skeleton.bones[2].rotation.z = -skeleton.bones[0].rotation.z / 2;
   checkForResize();
   controls.update();
 };
--- src/entities/wave.ts
+++ src/entities/wave.ts
@@ -1,47 +1,139 @@
 import * as THREE from "three";
 import { scene } from "../environment/renderer";
 
-//__________________________________  MAIN TUBE _________________________________________
+// //__________________________________  MAIN TUBE _________________________________________
 
-const sideLength = 500;
-const pathLength = 200;
-const generateSinPath = () => {
-  const pathPoints = Array.from({ length: pathLength }, (_, i) => {
-    return new THREE.Vector3(
-      i - pathLength / 2,
-      Math.sin(i - pathLength / 2) * 8 * Math.PI
-    );
-  });
+// const sideLength = 100;
+// const pathLength = 200;
+// const generateSinPath = () => {
+//   const pathPoints = Array.from({ length: pathLength }, (_, i) => {
+//     return new THREE.Vector3(
+//       i - pathLength / 2,
+//       Math.sin(i - pathLength / 2) * 8 * Math.PI
+//     );
+//   });
 
-  return pathPoints;
-};
+//   return pathPoints;
+// };
 
-generateSinPath();
+// generateSinPath();
 
-const path = new THREE.CatmullRomCurve3(generateSinPath());
-const geometry = new THREE.TubeGeometry(path, 64, 1, 64, false);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const curve = new THREE.SplineCurve([
+//   new THREE.Vector2(-10, 0),
+//   new THREE.Vector2(-5, 5),
+//   new THREE.Vector2(0, 0),
+//   new THREE.Vector2(5, -5),
+//   new THREE.Vector2(10, 0),
+// ]);
 
-//__________________________________ SIDE TUBES _________________________________________
+// const path = new THREE.CatmullRomCurve3(generateSinPath());
+// const geometry = new THREE.TubeGeometry(path, 64, 1, 64, false);
+// const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
-const pathStart = new THREE.CatmullRomCurve3(
-  Array.from(
-    { length: sideLength },
-    (_, i) => new THREE.Vector3(i - (sideLength + pathLength / 2), 0)
-  )
-);
-const startGeometry = new THREE.TubeGeometry(pathStart, 64, 1, 64, false);
-const startMesh = new THREE.Mesh(startGeometry, material);
-scene.add(startMesh);
+// //__________________________________ SIDE TUBES _________________________________________
 
-const pathEnd = new THREE.CatmullRomCurve3(
-  Array.from(
-    { length: sideLength },
-    (_, i) => new THREE.Vector3(i + pathLength / 2, 0)
-  )
-);
-const endGeometry = new THREE.TubeGeometry(pathEnd, 64, 1, 64, false);
-const endMesh = new THREE.Mesh(endGeometry, material);
-scene.add(endMesh);
+// const pathStart = new THREE.CatmullRomCurve3(
+//   Array.from(
+//     { length: sideLength },
+//     (_, i) => new THREE.Vector3(i - (sideLength + pathLength / 2), 0)
+//   )
+// );
+// const startGeometry = new THREE.TubeGeometry(pathStart, 64, 1, 64, false);
+// const startMesh = new THREE.Mesh(startGeometry, material);
+// scene.add(startMesh);
+
+// const pathEnd = new THREE.CatmullRomCurve3(
+//   Array.from(
+//     { length: sideLength },
+//     (_, i) => new THREE.Vector3(i + pathLength / 2, 0)
+//   )
+// );
+// const endGeometry = new THREE.TubeGeometry(pathEnd, 64, 1, 64, false);
+// const endMesh = new THREE.Mesh(endGeometry, material);
+// scene.add(endMesh);
+
+// var clock = new THREE.Clock(true);
+
+// export var n = 20,
+//   boneSize = 10,
+//   size = n * boneSize,
+//   bones = [];
+
+// for (var i = 0; i <= n; i++) {
+//   bones[i] = new THREE.Bone();
+//   if (i) bones[i - 1].add(bones[i]);
+//   bones[i].position.x = i ? boneSize : -size / 2;
+// }
+
+// export let skeleton = new THREE.Skeleton(bones);
+
+// // tapewrm skin
+
+// var geometry = new THREE.SphereGeometry(size / 2, 60, 120, 0, Math.PI)
+//     .rotateZ(Math.PI / 4)
+//     .scale(1, 0.07, 0.02),
+//   skinIndices = [],
+//   skinWeights = [];
+
+// var pos = geometry.getAttribute("position");
+// for (var i = 0; i < pos.count; i++) {
+//   var x = pos.getX(i) + size / 2,
+//     bone = Math.floor(Math.min(x / boneSize, n)),
+//     k = (x / boneSize) % 1;
+
+//   var cos = Math.cos(((Math.PI * 2) / 3) * (k - 0.5));
+
+//   if (k < 0.5) skinIndices.push(bone, Math.max(bone - 1, 0), 0, 0);
+//   else skinIndices.push(bone, Math.min(bone + 1, n), 0, 0);
+//   skinWeights.push(cos, 1 - cos, 0, 0);
+// }
+
+// geometry.setAttribute(
+//   "skinIndex",
+//   new THREE.Uint16BufferAttribute(skinIndices, 4)
+// );
+// geometry.setAttribute(
+//   "skinWeight",
+//   new THREE.Float32BufferAttribute(skinWeights, 4)
+// );
+
+// var material = new THREE.MeshPhongMaterial({
+//   color: "midnightblue",
+//   skinning: true,
+//   flatShading: true,
+//   shininess: 150,
+// });
+// export const tapeworm = new THREE.SkinnedMesh(geometry, material);
+// tapeworm.add(skeleton.bones[0]);
+// tapeworm.bind(skeleton);
+// tapeworm.position.x = size / 10;
+
+// scene.add(tapeworm);
+// Step 2: Create the Sine Wave
+// const numPoints = 1000; // Adjust this to control the smoothness of the wave
+// const amplitude = 1;    // Adjust this to control the height of the wave
+// const frequency = 3;    // Adjust this to control the number of waves
+
+// const points = [];
+// for (let i = 0; i < numPoints; i++) {
+//   const x = (i / numPoints) * 4 * Math.PI; // Adjust 4 * Math.PI to control the length of the wave
+//   const y = amplitude * Math.sin(frequency * x);
+//   const z = 0; // Adjust this if you want to create a 3D waveform
+
+//   points.push(new THREE.Vector3(x, y, z));
+// }
+
+// const tubeGeometry = new THREE.TubeGeometry(
+//   new THREE.CatmullRomCurve3(points),
+//   numPoints - 1,
+//   0.04, // Adjust this to control the thickness of the tube
+//   8,    // Adjust this to control the number of segments
+//   false
+// );
+
+// const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 }); // Adjust the color as desired
+
+// const sineWave = new THREE.Mesh(tubeGeometry, material);
+// scene.add(sineWave);
