54ec91b9261cbb2c2d6c0bb736bff7e348b9e2c5
aryan karim <aryanmajid97@gmail.com>
2023-07-26 23:49:43 +0300

    add monkey icon

--- src/entities/controller.ts
+++ src/entities/controller.ts
@@ -3,15 +3,20 @@ import { startRecording, stopRecording } from '../controllers/listeners/micropho
 let isOff = true
 
 const startBtnEl = document.getElementById('startBtn')
+const imgEl = document.createElement('img')
+imgEl.setAttribute('src', '/public/calm.svg')
+startBtnEl?.appendChild(imgEl)
 
 startBtnEl?.addEventListener('click', () => {
     if (isOff) {
         startRecording()
-        startBtnEl.innerText = 'Stop'
+        imgEl.setAttribute('src', '/public/dancing.svg')
+        imgEl.classList.toggle('dance')
         isOff = false
     } else {
         stopRecording()
-        startBtnEl.innerText = 'Start'
+        imgEl.setAttribute('src', '/public/calm.svg')
+        imgEl.classList.toggle('dance')
         isOff = true
     }
 })
