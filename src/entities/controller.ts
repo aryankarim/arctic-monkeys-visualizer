f2f17f8f56237d27c040255b83582a02f3ff969d
AryanKarim <aryanmajid97@gmail.com>
2023-08-05 20:32:50 +0300

    continue adding play and pause button

--- src/entities/controller.ts
+++ src/entities/controller.ts
@@ -1,42 +1,61 @@
 import { startRecording, stopRecording } from '../controllers/listeners/microphone'
 import { play, stop } from '../controllers/listeners/mp3Init'
 
-let isOff = true
+const sockAudioEl = document.getElementById('sock-audio-el') as HTMLAudioElement
+const randomAudioEl = document.getElementById('random-audio-el') as HTMLAudioElement
+const playPauseBtn = document.getElementById('play-pause-btn')
+const sockBtn = document.getElementById('sock-btn')
+const stereoBtn = document.getElementById('stereo-btn')
+const imgEl = document.querySelector('#start-btn > img') as HTMLImageElement
 
-const audioEl = document.getElementById('audioEl') as HTMLAudioElement
-const startBtnEl = document.getElementById('startBtn')
-const mp3Btn = document.getElementById('audioBtn')
-const imgEl = document.createElement('img')
-imgEl.setAttribute('src', '/calm.svg')
-startBtnEl?.appendChild(imgEl)
+enum Player {
+    idle = '',
+    mic = 'mic',
+    socks = 'socks',
+    random = 'random',
+}
 
-startBtnEl?.addEventListener('click', () => {
-    if (isOff) {
-        startRecording()
-        imgEl.setAttribute('src', '/dancing.svg')
-        imgEl.classList.toggle('dance')
-        isOff = false
-    } else {
+const stereo: {
+    playing: Player
+    reset: () => void
+} = {
+    playing: Player.idle,
+    reset() {
+        stereo.playing = Player.idle
+        // knee socks reset
+        stop()
+        sockAudioEl.pause()
+        // mic reset
         stopRecording()
-        imgEl.setAttribute('src', '/calm.svg')
-        imgEl.classList.toggle('dance')
-        isOff = true
-    }
+        imgEl.setAttribute('src', '/calm.png')
+        imgEl.classList.remove('dance')
+    },
+}
+
+playPauseBtn?.addEventListener('click', () => {
+    if (stereo.playing == Player.mic) return
+    stereo.playing = Player.mic
+    stereo.reset()
+    startRecording()
+    imgEl.setAttribute('src', '/dancing.png')
+    imgEl.classList.add('dance')
 })
 
-let playing = false
+sockBtn?.addEventListener('click', async () => {
+    console.log()
 
-mp3Btn?.addEventListener('click', async () => {
-    stopRecording()
-    imgEl.setAttribute('src', '/calm.svg')
-    isOff = true
-    if (!playing) {
-        playing = true
-        await play('/knee-socks.mp3')
-        audioEl.play()
-    } else {
-        playing = false
-        stop()
-        audioEl.pause()
-    }
+    if (stereo.playing == Player.socks) return
+    stereo.playing = Player.socks
+    stereo.reset()
+    await play('/knee-socks.mp3')
+    sockAudioEl.play()
+})
+
+stereoBtn?.addEventListener('click', async () => {
+    if (stereo.playing == Player.random) return
+    stereo.playing = Player.random
+    stereo.reset()
+    await play('/2.mp3')
+    randomAudioEl.src = '/2.mp3'
+    randomAudioEl.play()
 })
